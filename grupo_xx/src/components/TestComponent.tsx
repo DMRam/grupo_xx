d73c4f34eb1c8837156d4a4af46d9cb0dfd8b201
dannymunozramirez <danny.munoz.ramirez@gmail.com>
2024-03-22 11:58:07 -0400

    First commit from VS

---
+++ grupo_xx/src/components/TestComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from '../../node_modules/react-native/types/index'
+
+export const TestComponent = () => {
+    return (
+        <View>
+            <Text>
+
+                Test
+            </Text>
+        </View>
+    )
+}
